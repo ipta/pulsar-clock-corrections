111bacdafe69265064fc05f7002c00f198fdbe66
Anne Archibald <anne.archibald@ncl.ac.uk>
2022-05-24 15:10:37 +0100

    Initial summary table

--- NOTES.md
+++ NOTES.md
@@ -1,5 +1,5 @@
 Notes for future use
---------------------
+====================
 
 GBT clock corrections are available from
 https://www.gb.nrao.edu/~fghigo/timer/time_gbt.dat, updated approximately
@@ -25,3 +25,35 @@ on Github. But I do want to add a report generator, perhaps making a Markdown
 table, that could be used in such a setting; alternatively we might want each
 file's update to be a separate action so the success or failure could be
 reported through the driver software.
+
+Reporting
+---------
+
+github.io publishes markdown files as a web site; we could generate markdown files (including tables) to report the status of the clock corrections and check these in when we run updates.
+
+- State of the clock corrections
+    - Start and end date, cadence (typical, recent), last update time, last update status
+- Update report
+    - Status and error message for each file
+- Details about each file
+    - Update log, description, related files, plot of corrections?
+
+Clock correction objects
+------------------------
+
+- How updates were/are obtained: downloaded, converted, static
+    - Authoritative or secondary - did we get it from the observatory or some secondary location? Is this a placeholder?
+- Update logs - Failed with reason, unchanged, accepted automatically, accepted manually in spite of validation failure
+- Description of file and its origin/update method
+- Related files (and how related)
+- Canonical order
+- Nature (tempo/tempo2 clock correction, other)
+- Validation method/settings
+- Last major revision (replace clock files older than this regardless)
+- How often to check for updates
+
+Observatory contacts
+--------------------
+
+- FAST - Weiwei Zhu <zhuww@nao.cas.cn>
+- Effelsberg - Joris Verbiest
--- pulsar_clock_corrections.py
+++ pulsar_clock_corrections.py
@@ -1,62 +1,219 @@
-import numpy as np
+from io import StringIO
+import re
 import os
-from pint.observatory.clock_file import ClockFile
-from astropy.utils.data import download_file
 from pathlib import Path
+import inspect
+
+import numpy as np
+from astropy.time import Time
+from astropy.utils.data import download_file
+from pint.observatory.clock_file import ClockFile
+
 
 def base_location():
     return Path(__file__).parent
 
+
 def list_candidate_clock_files():
-    return sorted(base_location().glob("tempo/clock/time*_*.dat")) + sorted(base_location().glob("T2runtime/clock/*2*.clk"))
+    return sorted(base_location().glob("tempo/clock/time*_*.dat")) + sorted(
+        base_location().glob("T2runtime/clock/*2*.clk")
+    )
+
 
 class ValidationError(RuntimeError):
     pass
 
 
 class ClockFileUpdater:
-    def __init__(self, filename, download_url=None, format="tempo", bogus_last_entry=False, obscode=None):
+    def __init__(
+        self,
+        short_description,
+        filename,
+        authority="temporary",
+        download_url=None,
+        format="tempo",
+        bogus_last_entry=False,
+        obscode=None,
+        invalidate_if_older_than=None,
+        update_interval_days=7,
+        description="",
+    ):
         self.filename = filename
-        self.filepath = base_location()/self.filename
+        self.short_description = short_description
+        self.filepath = base_location() / self.filename
         self.format = format
         self.bogus_last_entry = bogus_last_entry
         self.download_url = download_url
         self.obscode = obscode
+        self.invalidate_if_older_than = None
+        self.update_interval_days = update_interval_days
+        self.description = inspect.cleandoc(description)
+        self._last_log_entry = None
+        self.log_entry_re = re.compile(r"([0-9 :.-]+) - ([^:]+)(: (.*))?")
+        self._clock_file = None
+
+    @property
+    def log_file(self):
+        return base_location() / "log" / (self.filename + ".log")
+
+    def add_to_log(self, msg):
+        entry = Time.now().iso + " - " + msg + "\n"
+        with open(self.log_file, "w+t") as f:
+            f.write(entry)
+        self._last_log_entry = entry
+
+    @property
+    def last_log_entry(self):
+        if self._last_log_entry is None:
+            self._last_log_entry = open(self.log_file, "rt").readlines()[-1]
+        return self._last_log_entry
+
+    def parse_log_entry(self, entry):
+        r = self.log_entry_re.match(entry.strip())
+        reason = r.group(4)
+        if reason is None:
+            reason = ""
+        return Time(r.group(1), format="iso"), r.group(2), reason
 
     def get(self, cache=False):
         return download_file(self.download_url, cache=cache)
 
-    def validate(self, old_file, new_file):
-        old = ClockFile.read(old_file, format=self.format, bogus_last_entry=self.bogus_last_entry, obscode=self.obscode)
-        new = ClockFile.read(new_file, format=self.format, bogus_last_entry=self.bogus_last_entry, obscode=self.obscode)
-        d = old.time != new.time[:len(old.time)]
+    @property
+    def clock_file(self):
+        if self._clock_file is None:
+            self._clock_file = ClockFile.read(
+                base_location() / self.filename,
+                format=self.format,
+                bogus_last_entry=self.bogus_last_entry,
+                obscode=self.obscode,
+            )
+        return self._clock_file
+
+    def validate(self, new_file):
+        old = self.clock_file
+        new = ClockFile.read(
+            new_file,
+            format=self.format,
+            bogus_last_entry=self.bogus_last_entry,
+            obscode=self.obscode,
+        )
+        d = old.time != new.time[: len(old.time)]
         if np.any(d):
-            raise ValidationError(f"New version of {old_file} MJDs differ from old version where they overlap in {np.sum(d)} places")
-        d = old.clock != new.clock[:len(old.clock)]
+            raise ValidationError(
+                f"New version of {old_file} MJDs differ from old version where they overlap in {np.sum(d)} places"
+            )
+        d = old.clock != new.clock[: len(old.clock)]
         if np.any(d):
-            raise ValidationError(f"New version of {old_file} clock corrections differ from old version where they overlap in {np.sum(d)} places")
+            raise ValidationError(
+                f"New version of {old_file} clock corrections differ from old version where they overlap in {np.sum(d)} places"
+            )
 
     def try_update(self, cache=False):
         try:
             f = self.get(cache=cache)
         except IOError as e:
-            return False, f"Failed to download {self.filename}: {e}"
+            self.add_to_log(f"Failed to download: {e}")
+            return False
         old_contents = self.filepath.read_text()
         new_contents = Path(f).read_text()
         if old_contents == new_contents:
-            return True, f"Downloaded file for {self.filename} is unchanged"
+            self.add_to_log(f"Unchanged")
+            return True
         try:
-            self.validate(base_location()/self.filename, f)
+            self.validate(f)
         except ValidationError as e:
-            return False, f"Downloaded file for {self.filename} couldn't be validated: {e}"
+            self.add_to_log(f"Validation failed: {e}")
+            return False
         self.filepath.write_text(new_contents)
-        return True, f"Updated {self.filename}"
+        self._clock_file = None
+        self.add_to_log(f"Updated")
+        return True
 
     def __repr__(self):
-        return f'{self.__class__.__name__}("{self.filename}")'
+        return (
+            f"{self.__class__.__name__}({self.short_description!r}, {self.filename!r})"
+        )
+
+
+tempo_repository_url = "https://raw.githubusercontent.com/nanograv/tempo/master/clock/"
 
 updaters = [
-    ClockFileUpdater("tempo/clock/time3_gbt.dat", download_url="https://www.gb.nrao.edu/~fghigo/timer/time_gbt.dat", format="tempo", obscode="3")
+    ClockFileUpdater(
+        "GBT",
+        "tempo/clock/time3_gbt.dat",
+        download_url="https://www.gb.nrao.edu/~fghigo/timer/time_gbt.dat",
+        authority="observatory",
+        format="tempo",
+        obscode="1",
+        update_interval_days=1,
+        description="""GBT clock correction file
+
+            The observatory distributes this file on the Web, updated about daily.
+
+            If questions arise, contact ???
+        """,
+    ),
+    ClockFileUpdater(
+        "Jodrell Bank",
+        "tempo/clock/time_jb.dat",
+        download_url=tempo_repository_url + "time_jb.dat",
+        authority="temporary",
+        format="tempo",
+        obscode="8",
+        bogus_last_entry=True,
+        description="""Jodrell Bank clock correction file
+
+            This file is pulled from the TEMPO repository and may not be fully up-to-date.
+        """,
+    ),
+    ClockFileUpdater(
+        "Arecibo",
+        "tempo/clock/time_ao.dat",
+        download_url=tempo_repository_url + "time_ao.dat",
+        authority="temporary",
+        format="tempo",
+        obscode="3",
+        update_interval_days=np.inf,
+        description="""Arecibo clock correction file
+
+            Since the telescope collapse, this file should not need additional updates.
+        """,
+    ),
 ]
 
 
+def try_all_updates():
+    for u in updaters:
+        u.try_update()
+
+
+def short_date(t):
+    return t.datetime.strftime('%Y-%m-%d')
+
+def updater_summary_table():
+    o = StringIO()
+    print(
+        f"| Name "
+        f"| File "
+        f"| Corrections start "
+        f"| Corrections end "
+        f"| Last check date "
+        f"| Last check result ",
+        file=o,
+    )
+    print(f"|:--- |:--- | --- | --- | --- |:--- ", file=o)
+    for u in updaters:
+        tstart = u.clock_file.time[0]
+        tend = u.clock_file.time[-2 if u.bogus_last_entry else -1]
+        last_date, result, details = u.parse_log_entry(u.last_log_entry)
+        print(
+            f"| {u.short_description} "
+            f"| {u.filename} "
+            f"| {short_date(tstart)} MJD {tstart.mjd:.1f} "
+            f"| {short_date(tend)} MJD {tend.mjd:.1f} "
+            f"| {short_date(last_date)} "
+            f"| {result} ",
+            file=o,
+        )
+    print(file=o)
+    return o.getvalue()
