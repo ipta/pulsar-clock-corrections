28c3912a2c9550382328eef8c65657553eae89cc
David Kaplan <dlakaplan@gmail.com>
2023-12-02 16:39:12 -0600

    fix so will not try forever to find a valid URL

--- pks.py
+++ pks.py
@@ -11,12 +11,15 @@ clockfilebaseurl = "https://www.parkes.atnf.csiro.au/observing/clockfiles/"
 def get_mostrecent_pks_url():
     startdate = datetime(Time.now().datetime.year, Time.now().datetime.month, 1)
     currdate = startdate
-    while True:
+    success = False
+    while currdate > datetime(2020, 1, 1):
         try:
             filename = f"pks2gps.clk.{currdate.strftime('%Y%m%d')}"
             url = f"{clockfilebaseurl}{filename}"
             f = astropy.utils.data.download_file(url, cache=True)
+            success = True
             break
         except IOError:
             currdate = (currdate + timedelta(days=-1)).replace(day=1)
-    return url
+
+    return url if success else None
